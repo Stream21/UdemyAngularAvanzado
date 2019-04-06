d70daf54acc205cacd0ddd0cd8dbc60441337cab
Antonio <lcs.Arjones@gmail.com>
2019-04-06 01:46:19 +0100

    Punto de guardado

--- src/app/shared/breadcrubs/breadcrubs.component.ts
+++ src/app/shared/breadcrubs/breadcrubs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from '../../../../node_modules/rxjs/operators';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrubs',
@@ -12,14 +12,23 @@ import { Title } from '@angular/platform-browser';
 export class BreadcrubsComponent implements OnInit {
   public titulo: string;
 
-  constructor(private router: Router,
-              private title: Title) {
+  constructor(
+    private router: Router,
+    private title: Title,
+    private meta: Meta
+    ) {
     this.getData().subscribe( event => {
         this.titulo = event.titulo;
         this.title.setTitle(this.titulo);
       });
 
-  }
+    const metaTag: MetaDefinition = {
+      name: 'description',
+      content: this.titulo
+    };
+
+    this.meta.updateTag( metaTag );
+    }
 
   ngOnInit() {
   }
